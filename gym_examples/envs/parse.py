eaccf0723aa17d64960dbc8e65eac2d5d38c5d59
jucaleb4 <calebju@gmail.com>
2024-04-04 15:33:45 -0400

    more robust way to loading in files

--- gym_examples/envs/parse.py
+++ gym_examples/envs/parse.py
@@ -22,6 +22,7 @@ def get_caiso_data(print_data=False):
     root = "/Users/calebju/Code/github/gym-examples/gym_examples/envs"
     # TODO: The location of `Github` needs to be changed on ad-hoc basis
     root = os.path.join(os.path.expanduser("~"), "gym-examples/gym_examples/envs")
+    root = os.path.dirname(os.path.abspath(__file__))
     if not os.path.exists(root):
         root = os.path.join(os.path.expanduser("~"), "Code", "github", "gym-examples/gym_examples/envs")
 
