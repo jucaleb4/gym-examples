0289a8bd462cacc9650598040c86199c01dde729
Caleb Ju <calebju4@gatech.edu>
2026-06-30 23:25:05 -0400

    Allow skipping of BatteryEnv if missing modules

--- gym_examples/envs/__init__.py
+++ gym_examples/envs/__init__.py
@@ -1,6 +1,9 @@
 from gym_examples.envs.grid_world import GridWorldEnv
 from gym_examples.envs.grid_world_model import GridWorldModelEnv
-from gym_examples.envs.battery_env import SimpleBatteryEnv
+try:
+    from gym_examples.envs.battery_env import SimpleBatteryEnv
+except ModuleNotFoundError as e:
+    print("Unable to import SimpleBatteryEnv (Error: %s)" % e)
 from gym_examples.envs.simple_world import SimpleWorldEnv
 from gym_examples.envs.lqr import LQREnv
 from gym_examples.envs.risk_adverse_portfolio import GiniPortfolioEnv
